23c4750daa4d1ab39feb2449312cf85add1dd437
Mônica Cotrim Manfrinato <monica.manfrido@aluno.senai.br>
2026-08-31 13:02:16 -0300

    🐛 - Estrutura base index.html

---
+++ src/scripts/script.js
@@ -0,0 +1,5 @@
+//Campo de formulário para doações
+//Cards com as fotinhas dos animais
+//Botão em cada card 'mais informações' e abre um pop-up com mais infromações
+//Menu sanduíche para 'animais' 'nos ajude'
+
